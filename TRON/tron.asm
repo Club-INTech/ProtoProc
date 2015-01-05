da487dcc9134c5ef6f39df7caba904fa6b591414
PF <pierre-francois.gimenez@telecom-sudparis.eu>
2015-01-05 21:26:37 +0100

    Code assembleur écrit

--- TRON/tron.asm
+++ TRON/tron.asm
@@ -1,16 +1,16 @@
-;espace mémoire:
-;
+; organisation de la mémoire graphique:
+; XXX XXX XXY YYY YYY -> valeur
+; 
 
-#define LOG_TAILLE_X 2
-#define LOG_TAILLE_Y 2
-#define LOG_LONGUEUR 2
+#define LOG_TAILLE_X 8 ; 256
+#define LOG_TAILLE_Y 7 ; 128
+#define LOG_LONGUEUR 2 ; longueur de la queue
 #define TAILLE_Y (1<<LOG_TAILLE_Y) ;hauteur de TAILLE_Y
 #define TAILLE_X (1<<LOG_TAILLE_X) ;largeur de TAILLE_X
-#define LONGUEUR (1<<LOG_LONGUEUR) ;longueur de LONGUEUR
+#define LONGUEUR (1<<LOG_LONGUEUR)
 #define NB_JOUEURS 1 ;nb_joueurs
-#define TRUE 1
-#define FALSE 0
-#define TAILLE_WORD 2 ;nombre de bit dans un mot machine
+#define TAILLE_WORD 3 ;nombre de bit dans un mot machine
+#define MUR (NB_JOUEURS+1)
 
 #define WORD_PAR_X (LOG_TAILLE_X/TAILLE_WORD)+1
 #define WORD_PAR_Y (LOG_TAILLE_Y/TAILLE_WORD)+1
@@ -19,8 +19,9 @@
 
 ; "table" a besoin de 2 valeurs différentes pour juste jouer (vide / plein), mieux vaut NB_JOUEURS+2 valeurs pour l'affichage graphique (1 couleur pour vide, 1 couleur pour plein, 1 couleur par joueur).
 ; du coup, il faudrait au moins 3 bits pour stocker chaque valeur
+
 ; il y a trois indices: un pour X, un pour Y, un pour le mot désiré
-; l'organisation est faite ainsi: XXXYYYWWW (avec "LOG_TAILLE_X" fois X, "LOG_TAILLE_Y" fois Y, 3 fois W)
+; l'organisation est faite ainsi: XXXYYY (avec "LOG_TAILLE_X" fois X, "LOG_TAILLE_Y" fois Y)
 ; "table" doit être aligné sur une puissance de 2 plus grande que LOG_TAILLE_X + LOG_TAILLE_Y + 3
 
 table dw (1 << (LOG_TAILLE_X+LOG_TAILLE_Y+3)) dup (?)
@@ -29,10 +30,11 @@ table dw (1 << (LOG_TAILLE_X+LOG_TAILLE_Y+3)) dup (?)
 ; il y a 4 indices dans ce tableau.
 ; J: l'indice du joueur, entre 0 et NB_JOUEURS - 1 (max 4 joueurs). Tient sur 2 bits.
 ; Q: l'indice de la longueur de la queue, entre 0 et LONGUEUR-1. Tient sur LOG_LONGUEUR bits.
-; A: l'indice d'abscisse ou d'ordonnée (0: abscisse, 1: ordonnée). Tient sur 1 bit.
-; W: l'indice à l'intérieur de l'abscisse (ou de l'ordonnée) elle-même, entre 0 et NB_WORD_POUR_POSITION-1. Tient probablement sur 2 bits
-; l'organisation est faite ainsi: JJ   QQQ   QQQ   WWA
+; W: l'indice à l'intérieur de l'abscisse (ou de l'ordonnée) elle-même, entre 0 et NB_WORD_POUR_POSITION-1. Tient sur 3 bits
+; l'organisation est faite ainsi: JJ   QQQ   QQQ   WWW
 ;                                  3     2     1     0
+
+; pour indices: 100 000 0JJ WWW
 
 joueurs dw 1 << (1+2+2+LOG_LONGUEUR)) dup (?)
 indices dw (NB_JOUEURS) dup (?)
@@ -46,7 +48,8 @@ dir dw 0, 1, 0, -1, 0
 
 ; index prend les valeurs de 0 à TAILLE_Y*TAILLE_X-1
 mov index, TAILLE_Y*TAILLE_X ; i registre pour parcourir toute la table
-mov base, table ; table est une adresse, constante
+mov base, table ; table est une adresse, constante. Attention, on utilise un OU logique après, il faut donc
+; que l'utilisation des bits soit bien répartie entre l'index et la base
 boucleInitTable:
 dec index
 mov [base|index], 0
@@ -58,6 +61,8 @@ jnz index, boucleInitTable
 mov i, NB_JOUEURS
 bouclePourToutJoueur:
 dec i
+
+; LIGNES 
 
 ; j prend les valeurs de 1 à LONGUEUR-1
 mov base1, ((LONGUEUR-1) & 7)+1 ; mot faible de LONGUEUR-1
@@ -86,12 +91,13 @@ index, registre de 5*3 bits
 index est le mot de poids faible, jusqu'à index4, le mot de poids fort.
 flags: nul (ou non-nul) = overflow d'incrémentation, overflow dec (vrai si le mot vaut 111)
 
-mov base, 666444222000
+mov index, 888666444222000
 peut s'écrire en assembleur sans macro:
-mov base0, 000
-mov base1, 222
-mov base2, 444
-mov base3, 666
+mov index0, 000
+mov index1, 222
+mov index2, 444
+mov index3, 666
+mov index4, 888
 
 jnz reg, adr
 peut s'écrire en assembleur sans macro:
@@ -126,28 +132,68 @@ xor ACC, reg2
 cmp ACC
 jnz adr
 
+or ACC, imm
+peut s'écrire en assembleur sans macro:
+not ACC
+and ACC, ~imm
+not ACC
+
 mov reg, imm
 peut s'écrire en assembleur sans macro:
 mov ACC, imm
 mov reg, ACC
 
+mov [index], reg1:reg2
+qui envoie un double-mot (reg1 poids fort, reg2 poids faible) (little endian)
+peut s'écrire en assembleur sans macro:
+mov [index], reg2
+inc index
+mov [index], reg1
+dec index
+
+mov [index], reg1:reg2:reg3
+qui envoie un triple-mot (reg1 poids fort, reg3 poids faible) (little endian)
+peut s'écrire en assembleur sans macro:
+mov [index], reg3
+inc index
+mov [index], reg2
+inc index
+mov [index], reg1
+dec index
+dec index
+
+mov reg1:reg2:reg3, [index]
+qui lit un triple-mot (reg1 poids fort, reg3 poids faible) (little endian)
+peut s'écrire en assembleur sans macro:
+mov reg3, [index]
+inc index
+mov reg2, [index]
+inc index
+mov reg1, [index]
+dec index
+dec index
+
 Instructions nécessaires:
 mov ACC, imm
 mov reg, ACC
-mov [index], reg
+mov reg1, reg2
+movg [index], reg ; mov graphique: envoie la valeur au processeur graphique
+movg reg, [index] ;
+movd [index], reg ; mov data
+movd reg, [index] ; mov data
 inc reg
-cpinc reg ; conditional positive incrementation
-cninc reg ; condition negative incrementation
-not reg
+dec reg
+cinc reg ; conditional incrementation
+cdec reg ; condition decrementation
+cxor (reg) // effectue xor reg, 1 si overflow précédent
 cmp reg
-jnz add
-xor ACC, reg1 ; comparaison
-and ACC, imm ; pour les calculs de modulo
-
+jnz adr
+jmp adr
+rnd reg1 ; valeur aléatoire dans reg1 (convertisseur analogique numérique relié à une antenne dont on prend les bits de poids faible)
 
 composition instruction:
-code instruction
-imm (3)
-reg (3 ou 4)
-adr(16)
--> en codant une instruction sur 4 octets, on peut facilement avoir quelque chose de pas mal
+code instruction (4)
+imm ou reg ou adr (4)
+imm ou reg ou adr (4)
+imm ou reg ou adr (4)
+-> en codant une instruction sur 2 octets, on peut facilement avoir quelque chose de pas mal
--- TRON/tron/main.c
+++ TRON/tron/main.c
@@ -37,8 +37,14 @@ int main()
     {
         for(i = 0; i < NB_JOUEURS; i++)
         {
+
+mov reg0, NB_JOUEURS
+boucleJeu:
+dec reg0
+
             affiche();
             scanf("%d", &in);
+
             if(in == 8)
                 direction[i] = HAUT;
             else if(in == 4)
@@ -48,19 +54,107 @@ int main()
             else if(in == 2)
                 direction[i] = BAS;
 
+// on n'aura qu'à lire les registres qui contiennent les valeurs. On fera en sorte qu'électriquement, le registre conserve la dernière valeur non nulle
+
             int old_x = joueurs[i][indice[i]][0];
             int old_y = joueurs[i][indice[i]][1];
 
+mov index0, 0
+mov index1, reg0
+mov index2, 0
+mov index3, 4
+movd reg2:reg1, [index] // reg3:reg2 contient indice[i]
+// index0 vaut 0
+mov index1, reg1
+mov index2, reg2
+mov index3, reg0
+movd reg7:reg6:reg5:reg4:reg3, [index] // ils contiennent les anciennes valeurs de X et Y
             ok |= ((indice[i]+1) == LONGUEUR);
 
             indice[i]++;
             indice[i] %= LONGUEUR;
 
+inc reg1
+cinc reg2 // incrémentation de indice[i]
+// le modulo est automatique
+
+mov index0, 0
+mov index1, reg0
+mov index2, 0
+mov index3, 4
+movd [index], reg2:reg1 // on enregistre la nouvelle valeur
+
+jnz reg3, pasNouveauOk
+jnz reg2, pasNouveauOk
+mov reg8, 0 // nul: ok est vrai
+// (ne pas oublier d'initialiser reg10 à une valeur non nulle)
+
+pasNouveauOk:
+jnz reg8, pasOk
+
             if(ok)
                 table[joueurs[i][indice[i]][0]][joueurs[i][indice[i]][1]] = 0;
 
+mov index0, 0
+mov index1, reg2
+mov index2, reg3
+mov index3, reg0
+movd index, [index] // on récupère l'abscisse à cet indice
+movg [index], 0 // et on y met 0
+
+pasOk: // on a fini la condition
+
             joueurs[i][indice[i]][0] = (old_x+dir_x[direction[i]]+LARGEUR)%LARGEUR;
             joueurs[i][indice[i]][1] = (old_y+dir_x[direction[i]+1]+HAUTEUR)%HAUTEUR;
+
+mov reg1, INJ1 // input joueur 1
+jnz reg1, pas0
+
+//00 : HAUT
+inc reg3
+cinc reg4
+cxor reg5
+jmp finCondition
+
+pas0:
+dec reg1
+jnz reg1, pas1
+//01 : BAS
+dec reg3
+cdec reg4
+cxor reg5, 1
+jmp finCondition
+
+pas1:
+dec reg1
+jnz reg1, pas2
+//10 : GAUCHE
+dec reg5
+cdec reg6
+cdec reg7
+dec reg5
+cdec reg6
+cdec reg7
+jmp finCondition
+
+pas2:
+// du coup, c'est forcément 3
+//11 : DROITE
+inc reg5
+cinc reg6
+cinc reg7
+inc reg5
+cinc reg6
+cinc reg7
+
+finCondition:
+
+mov index0, 0
+mov index1, reg2
+mov index2, reg3
+mov index3, reg0
+
+movd [index], reg7:reg6:reg5:reg4:reg3
 
             if(table[joueurs[i][indice[i]][0]][joueurs[i][indice[i]][1]] != 0)
             {
@@ -68,13 +162,32 @@ int main()
                 return EXIT_SUCCESS;
             }
 
+mov index0, reg3
+mov index1, reg4
+mov index2, reg5
+mov index3, reg6
+mov index4, reg7
+
+movg reg1, [index]
+jnz finJeu
+
             table[joueurs[i][indice[i]][0]][joueurs[i][indice[i]][1]] = i+1;
 
+mov reg2, reg0
+inc reg2
+movg [index], reg2
+
         }
+jnz reg0, boucleInitJoueurs
+mov reg0, NB_JOUEURS
+jmp boucleInitJoueurs
     }
 
     return 0;
 }
+
+finJeu: 
+TODO...
 
 void affiche()
 {
@@ -98,31 +211,89 @@ void affiche()
 
 void init()
 {
+
+mov reg8, 1
     // initialisation de la table (sans obstacles ici)
     for(i = 0; i < LARGEUR; i++)
         for(j = 0; j < HAUTEUR; j++)
             table[i][j] = 0;
+
+mov index, 0 ;taille tableau = indice max + 1 = overflow, on retombe à 0
+boucleInitTable:
+dec index
+movg [index], 0
+cmp index
+jnz boucleInitTable
 
     srand(time(NULL));
 
     for(i = 0; i < 10; i++)
         table[rand()%LARGEUR][rand()%HAUTEUR] = 255;
 
+mov reg0, 10 // valeur à ajuster...
+mov reg1, MUR
+boucleInitTableRandom:
+rnd index0
+rnd index1
+rnd index2
+rnd index3
+rnd index4
+movg [index], reg1
+dec reg0
+cmp reg0
+jnz boucleInitTableRandom
+
+
     for(i = 0; i < NB_JOUEURS; i++)
     {
+mov reg0, NB_JOUEURS
+boucleInitJoueurs:
+dec reg0
+
         do {
             // init des positions des joueurs
             joueurs[i][0][0] = rand()%LARGEUR;
             joueurs[i][0][1] = rand()%HAUTEUR;
         } while(table[joueurs[i][0][0]][joueurs[i][0][1]] != 0);
-
         table[joueurs[i][0][0]][joueurs[i][0][1]] = i+1; // on note l'obstacle
+
+boucleInitPositionJoueur:
+rnd index0
+rnd index1
+rnd index2
+rnd index3
+rnd index4
+movg reg1, [index]
+cmp reg1
+jnz boucleInitPositionJoueur
+movg [index], reg0 // reg0 contient le numéro du joueur
+mov reg1, index0
+mov reg2, index1
+mov reg3, index2
+mov reg4, index3
+mov reg5, index4
+
+
 
         for(j = 1; j < LONGUEUR; j++)
         {
             joueurs[i][j][0] = joueurs[i][0][0];
             joueurs[i][j][1] = joueurs[i][0][1];
         }
+
+mov index0, 0 // pour l'abscisse
+mov index1, 0
+mov index2, 0 // dernier élément de la queue + 1
+mov index3, reg0 // numéro du joueur
+boucleInitQueue:
+dec index1
+cdec index2
+movd [index], reg5:reg4:reg3:reg2:reg1
+jnz index1, boucleInitQueue
+jnz index2, boucleInitQueue
+
+jnz reg0, boucleInitJoueurs
+
     }
 
 }
