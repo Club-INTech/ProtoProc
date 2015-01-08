3b8ebdffa0fef60fa83d0ef7c14a0ab665a2ae2c
PF <pierre-francois.gimenez@telecom-sudparis.eu>
2015-01-08 20:20:49 +0100

    Documentation

--- TRON/tron.asm
+++ TRON/tron.asm
@@ -1,178 +1,199 @@
-; organisation de la mémoire graphique:
-; XXX XXX XXY YYY YYY -> valeur
-; 
+NB_JOUEURS EQU #1
+JOUEUR_ACTUEL EQU r0
+MUR EQU #7
+NB_MURS EQU #10
+INDICES_ARRAY EQU #4
+INDICE_HIGH EQU r1
+INDICE_LOW EQU r2
+XY0 EQU r3
+XY1 EQU r4
+XY2 EQU r5
+XY3 EQU r6
+XY4 EQU r7
 
-#define LOG_TAILLE_X 8 ; 256
-#define LOG_TAILLE_Y 7 ; 128
-#define LOG_LONGUEUR 2 ; longueur de la queue
-#define TAILLE_Y (1<<LOG_TAILLE_Y) ;hauteur de TAILLE_Y
-#define TAILLE_X (1<<LOG_TAILLE_X) ;largeur de TAILLE_X
-#define LONGUEUR (1<<LOG_LONGUEUR)
-#define NB_JOUEURS 1 ;nb_joueurs
-#define TAILLE_WORD 3 ;nombre de bit dans un mot machine
-#define MUR (NB_JOUEURS+1)
+; INITIALISATION
 
-#define WORD_PAR_X (LOG_TAILLE_X/TAILLE_WORD)+1
-#define WORD_PAR_Y (LOG_TAILLE_Y/TAILLE_WORD)+1
-; nombre de mots nécessaires pour coder une abscisse ou une ordonnnée
-; par exemple, si on a des mots de 3 bits, et qu'il y a 2^7 = 128 cases en largeur (ou hauteur), il faut 7/3+1=3 mots pour coder l'abscisse (ou l'ordonnée)
+; nécessaire pour "ok"
+mov reg8, #1
 
-; "table" a besoin de 2 valeurs différentes pour juste jouer (vide / plein), mieux vaut NB_JOUEURS+2 valeurs pour l'affichage graphique (1 couleur pour vide, 1 couleur pour plein, 1 couleur par joueur).
-; du coup, il faudrait au moins 3 bits pour stocker chaque valeur
+; on remet à la table à 0
+mov index0, #0
+mov index1, #0
+mov index2, #0
+mov index3, #0
+mov index4, #0
+mov r0, #0
+	boucleInitTable:
+dec index0
+cdec index1
+cdec index2
+cdec index3
+cdec index4
+movg [index], r0
+jnz index, boucleInitTable
 
-; il y a trois indices: un pour X, un pour Y, un pour le mot désiré
-; l'organisation est faite ainsi: XXXYYY (avec "LOG_TAILLE_X" fois X, "LOG_TAILLE_Y" fois Y)
-; "table" doit être aligné sur une puissance de 2 plus grande que LOG_TAILLE_X + LOG_TAILLE_Y + 3
+mov r0, NB_MURS
+mov r1, MUR
+	boucleInitTableRandom:
+rnd index0
+rnd index1
+rnd index2
+rnd index3
+rnd index4
+movg [index], r1
+dec r0
+jnz r0, boucleInitTableRandom
 
-table dw (1 << (LOG_TAILLE_X+LOG_TAILLE_Y+3)) dup (?)
+mov JOUEUR_ACTUEL, NB_JOUEURS
+	boucleInitJoueurs:
+dec JOUEUR_ACTUEL
 
-; joueurs
-; il y a 4 indices dans ce tableau.
-; J: l'indice du joueur, entre 0 et NB_JOUEURS - 1 (max 4 joueurs). Tient sur 2 bits.
-; Q: l'indice de la longueur de la queue, entre 0 et LONGUEUR-1. Tient sur LOG_LONGUEUR bits.
-; W: l'indice à l'intérieur de l'abscisse (ou de l'ordonnée) elle-même, entre 0 et NB_WORD_POUR_POSITION-1. Tient sur 3 bits
-; l'organisation est faite ainsi: JJ   QQQ   QQQ   WWW
-;                                  3     2     1     0
+	boucleInitPositionJoueur:
+rnd index0
+rnd index1
+rnd index2
+rnd index3
+rnd index4
+movg reg1, [index]
+jnz reg1, boucleInitPositionJoueur
+movg [index], JOUEUR_ACTUEL
+mov r1, index0
+mov r2, index1
+mov r3, index2
+mov r4, index3
+mov r5, index4
 
-; pour indices: 100 000 0JJ WWW
+mov index1, #0
+mov index2, #0 // dernier élément de la queue + 1
+mov index3, JOUEUR_ACTUEL // numéro du joueur
+; la mémoire vive est adressé sur 12 bits, donc index4 est non utilisé
+boucleInitQueue:
+mov index0, #0 // pour l'abscisse
+dec index1
+cdec index2
+movd [index], r1
+inc index0
+movd [index], r2
+inc index0
+movd [index], r3
+inc index0
+movd [index], r4
+inc index0
+movd [index], r5
+inc index0
+jnz index1, boucleInitQueue
+jnz index2, boucleInitQueue
 
-jnz reg, adr
-peut s'écrire en assembleur sans macro:
-cmp reg : met à jour les flags
-jnz adr : consulte les flags pour sauter
+jnz JOUEUR_ACTUEL, boucleInitJoueurs
 
-dec reg
-peut s'écrire en assembleur sans macro:
-neg reg
-inc reg
-neg reg
-qui peut encore s'écrire:
-not reg
-inc reg
-inc reg
-not reg
-inc reg
+mov JOUEUR_ACTUEL, NB_JOUEURS
+boucleJeu:
+dec JOUEUR_ACTUEL
 
-jmp adr
-peut s'écrire en assembleur sans macro:
-cmp reg
-jnz adr
-not reg
-cmp reg
-not red
-jnz adr
+mov index0, #0
+mov index1, JOUEUR_ACTUEL
+mov index2, #0
+mov index3, INDICES_ARRAY
+movd INDICE_LOW, [index]
+inc index0
+movd INDICE_HIGH, [index]	; INDICE_HIGH:INDICE_LOW contient indice[i]
+dec index0
+; index0 est nul
+mov index1, INDICE_LOW
+mov index2, INDICE_HIGH
+mov index3, JOUEUR_ACTUEL
+movd reg7:reg6:reg5:reg4:reg3, [index] ; ils contiennent les anciennes valeurs de X et Y
 
-jne reg1, reg2, adr
-peut s'écrire en assembleur sans macro:
-mov ACC, reg1
-xor ACC, reg2
-cmp ACC
-jnz adr
+inc reg1
+cinc reg2 // incrémentation de indice[i]
+// le modulo est automatique
 
-or ACC, imm
-peut s'écrire en assembleur sans macro:
-not ACC
-and ACC, ~imm
-not ACC
+mov index0, 0
+mov index1, JOUEUR_ACTUEL
+mov index2, 0
+mov index3, 4
+movd [index], reg2:reg1 // on enregistre la nouvelle valeur
 
-mov reg, imm
-peut s'écrire en assembleur sans macro:
-mov ACC, imm
-mov reg, ACC
+jnz reg3, pasNouveauOk
+jnz reg2, pasNouveauOk
+mov reg8, 0 // nul: ok est vrai
+// (ne pas oublier d'initialiser reg10 à une valeur non nulle)
 
-mov [index], reg1:reg2
-qui envoie un double-mot (reg1 poids fort, reg2 poids faible) (little endian)
-peut s'écrire en assembleur sans macro:
-mov [index], reg2
-inc index
-mov [index], reg1
-dec index
+pasNouveauOk:
+jnz reg8, pasOk
 
-mov [index], reg1:reg2:reg3
-qui envoie un triple-mot (reg1 poids fort, reg3 poids faible) (little endian)
-peut s'écrire en assembleur sans macro:
-mov [index], reg3
-inc index
-mov [index], reg2
-inc index
-mov [index], reg1
-dec index
-dec index
+mov index0, 0
+mov index1, reg2
+mov index2, reg3
+mov index3, JOUEUR_ACTUEL
+movd index, [index] // on récupère l'abscisse à cet indice
+movg [index], 0 // et on y met 0
 
-mov reg1:reg2:reg3, [index]
-qui lit un triple-mot (reg1 poids fort, reg3 poids faible) (little endian)
-peut s'écrire en assembleur sans macro:
-mov reg3, [index]
-inc index
-mov reg2, [index]
-inc index
-mov reg1, [index]
-dec index
-dec index
+pasOk: // on a fini la condition
 
-Instructions nécessaires:
-//mov ACC, imm
-mov reg, imm
-mov reg1, reg2
-movg [index], reg ; mov graphique: envoie la valeur au processeur graphique
-movg reg, [index] ;
-movd [index], reg ; mov data
-movd reg, [index] ; mov data
-inc reg
-dec reg
-cinc reg ; conditional incrementation
-cdec reg ; conditional decrementation
-cxor reg // effectue "xor reg, 1" si overflow précédent (inverse le bit de poids faible)
-cmp reg
-jnz adr
-jnz reg, adr ? (a priori faisable)
-jmp adr
-rnd reg1 ; valeur aléatoire dans reg1 (convertisseur analogique numérique relié à une antenne dont on prend les bits de poids faible)
+mov reg1, INJ1 // input joueur 1
+jnz reg1, pas0
 
-composition instruction:
-code instruction (4)
-imm ou reg ou adr (4)
-imm ou reg ou adr (4)
-imm ou reg ou adr (4)
--> en codant une instruction sur 2 octets, on peut facilement avoir quelque chose de pas mal
+//00 : HAUT
+inc reg3
+cinc reg4
+cxor reg5
+jmp finCondition
 
-Code instruction: (8 bits)
-memoire graphique en écriture et met index sur le bus d'adresse
-memoire graphique en lecture et met index sur le bus d'adresse
-memoire data en écriture
-memoire data en lecture
-program counter en écriture
-annule si overflow précédent (désactive l'écriture)
-commande ALU 1, dirige vers INC/DEC ou NOT
-commande ALU 2, transforme le INC en DEC (DEC REG = ADD REG, (MAX-1))
+pas0:
+dec reg1
+jnz reg1, pas1
+//01 : BAS
+dec reg3
+cdec reg4
+cxor reg5, 1
+jmp finCondition
 
-Registre 1, en écriture (4 bits)
-Registre 2, en lecture pour l'instruction et écriture pour la valeur immédiate (4 bits)
-La valeur immédiate (3 bits)
-Adresse poids fort (5 bits)
-Adresse poids faible (8 bits)
+pas1:
+dec reg1
+jnz reg1, pas2
+//10 : GAUCHE
+dec reg5
+cdec reg6
+cdec reg7
+dec reg5
+cdec reg6
+cdec reg7
+jmp finCondition
 
-mov reg1 reg2 imm : met imm dans ACC, puis reg2 dans reg1
-c'est-à-dire que:
-mov reg imm est en fait mov reg ACC imm
-mov reg1, reg2 est en fait mov reg1 reg2 0
-movg [index], reg1 est en fait movg ACC, reg1, 0 (avec l'écriture en mémoire graphique activée)
-le 1er registre met à jour les flags, toujours (après sa modification)
+pas2:
+// du coup, c'est forcément 3
+//11 : DROITE
+inc reg5
+cinc reg6
+cinc reg7
+inc reg5
+cinc reg6
+cinc reg7
 
-Il faut:
-- un bus de commande de 8 bits pour le code d'instruction
-- deux bus d'adresses (un d'écriture, un de lecture) de 3 bits pour les registres
-- un bus de donnée de 3 bits
+finCondition:
 
-Exemple de l'exécution d'une instruction:
-lecture du premier octet dans la mémoire programme sur un bus 8 bits
-on met en écriture un registre 8 bits qui contiendra le code d'instruction
-incrémentation de l'IP
-lecture du 2e octet, écriture, incrémentation de l'IP...
-lecture du 3e octet, écriture, incrémentation de l'IP...
-lecture du 4e octet, écriture, incrémentation de l'IP
+mov index0, 0
+mov index1, reg2
+mov index2, reg3
+mov index3, JOUEUR_ACTUEL
 
-on met la valeur immédiate dans le bus de donnée, on active l'écriture de ACC
-on met sur le bus de commande les 8 bits de l'instruction
-on met sur le bus d'adresse de lecture le registre 2, ce qui effectue les calculs et stock les résultats dans le registre tampon de l'ALU
-on met sur le bus d'adresse d'écriture le registre 1, on met sur le bus d'adresse en lecture le registre tampon de l'ALU
+movd [index], reg7:reg6:reg5:reg4:reg3
+
+mov index0, reg3
+mov index1, reg4
+mov index2, reg5
+mov index3, reg6
+mov index4, reg7
+
+movg reg1, [index]
+jnz finJeu
+
+mov reg2, JOUEUR_ACTUEL
+inc reg2
+movg [index], reg2
+
+jnz JOUEUR_ACTUEL, boucleInitJoueurs
+mov JOUEUR_ACTUEL, NB_JOUEURS
+jmp boucleInitJoueurs
+
